ed46c7228456af3d2a75b44098f5472c1163a19a
Ross Girshick <rbg@eecs.berkeley.edu>
2014-03-15 16:30:07 -0700

    show how to use RCNN_CONFIG_OVERRIDE

--- experiments/rcnn_run_pool5_explorer.m
+++ experiments/rcnn_run_pool5_explorer.m
@@ -1,6 +1,19 @@
 function rcnn_run_pool5_explorer()
 
-VOCdevkit = '/work4/rbg/VOC2007/VOCdevkit';
+% Example of how to use RCNN_CONFIG_OVERRIDE in a function
+%global RCNN_CONFIG_OVERRIDE;
+%conf_override.exp_dir = './cachedir/rcnn/v1.1/pool5';
+%RCNN_CONFIG_OVERRIDE = @() conf_override;
+
+% -------------------- CONFIG --------------------
+cache_name = 'v1_finetune_voc_2007_trainval_iter_70k';
+
+% change to point to your VOCdevkit install
+VOCdevkit = './datasets/VOCdevkit2007';
+% ------------------------------------------------
 
 imdb = imdb_from_voc(VOCdevkit, 'test', '2007');
-pool5_explorer(imdb, 'v1_finetune_voc_2007_trainval_iter_70000');
+
+pool5_explorer(imdb, cache_name);
+
+clear global RCNN_CONFIG_OVERRIDE;
