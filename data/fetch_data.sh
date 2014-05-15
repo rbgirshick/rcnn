e6f89ddf371c1bfd8abc92d4053518359c5bf52d
Ross Girshick <rbg@eecs.berkeley.edu>
2014-05-14 21:57:55 -0700

    README and fetch script for ilsvrc data package

--- data/fetch_data.sh
+++ data/fetch_data.sh
@@ -3,12 +3,23 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-echo "Downloading the R-CNN data package (precomputed models)..."
+echo "Downloading the PASCAL VOC R-CNN data package (precomputed models)..."
 
 wget http://www.cs.berkeley.edu/~rbg/r-cnn-release1-data.tgz
 
 echo "Unzipping..."
 
 tar zxvf r-cnn-release1-data.tgz && rm -f r-cnn-release1-data.tgz
+
+echo "Done."
+
+
+echo "Downloading the ILSVRC2013 R-CNN data package (precomputed models)..."
+
+wget http://www.cs.berkeley.edu/~rbg/r-cnn-release1-data-ilsvrc2013.tgz
+
+echo "Unzipping..."
+
+tar zxvf r-cnn-release1-data-ilsvrc2013.tgz && rm -f r-cnn-release1-data-ilsvrc2013.tgz
 
 echo "Done."
